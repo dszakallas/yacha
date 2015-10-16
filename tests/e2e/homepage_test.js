2e723a6e12365c6769d76c4f7e1bed6289ee9036
Midiparse <david.szakallas@gmail.com>
2015-10-16 21:50:09 +0200

    React boilerplate

--- tests/e2e/homepage_test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'Does it work?' : function (browser) {
-    browser
-      .url('http://localhost:8080')
-      .waitForElementVisible('body', 1000)
-      .assert.containsText('body', 'It works!')
-      .end();
-  }
-};
