77df8899b4cf043b947a6610e1b5255410aac101
Midiparse <david.szakallas@gmail.com>
2015-11-21 20:18:04 +0100

    Start wiring login screens

---
+++ src/core/ApiClient.js
@@ -0,0 +1,54 @@
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
+
+import request from 'superagent';
+
+function apiUrl(path) {
+    return `/api${path}`;
+}
+
+const ApiClient = {
+
+  login: (username, password) => new Promise((resolve, reject) => {
+    request
+      .post(apiUrl('/login'))
+      .send({username: username, password: password})
+      /*.accept('application/json')*/
+      .end((err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res.body);
+        }
+      });
+  }),
+
+  get: path => new Promise((resolve, reject) => {
+    request
+      .get(apiUrl(path))
+      /*.accept('application/json')*/
+      .end((err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res.body);
+        }
+      });
+  }),
+
+  post: (path, data) => new Promise((resolve, reject) => {
+    request
+      .post(apiUrl(path))
+      /*.accept('application/json')*/
+      .send(data)
+      .end((err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res.body);
+        }
+      });
+  }),
+
+};
+
+export default ApiClient;
